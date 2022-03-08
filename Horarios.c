df1c39be55ef23248fc47d8fa75491da15bd6361
David Brea Piñero <100689419+davidbp10@users.noreply.github.com>
2022-03-08 22:25:24 +0100

    Add files via upload
    Fichero de cabecera necesario para el módulo Horarios incluyendo los correspondientes tipos de datos y los prototipos de las funciones. También se ha comenzado la implementación de los ficheros con código (ficheros con extensión .c) de dicho módulo.

---
+++ Horarios.c
@@ -0,0 +1,17 @@
+//Implementación módulo Horarios
+
+#include <stdio.h>
+#include "Horarios.h"
+
+void annadirHoras(){
+	printf("Estas en annadir horas.\n");
+}
+void eliminarHoras(){
+	printf("Estas en eliminar horas.\n");
+}
+void modificarHoras(){
+	printf("Estas en modificar horas.\n");
+}
+void listarHorarios(){
+	printf("Estas en listar horas.\n");
+}
---
+++ Horarios.h
@@ -0,0 +1,35 @@
+//Declaraciones módulo Horarios.
+
+#ifndef _HORARIOS_H_
+#define _HORARIOS_H_
+
+typedef struct {
+	char id_prof[4];		//Identificador del profesor (Id_profesor), tres dígitos (debe coincidir con un identificador deusuario, Id_usuario, con perfil profesor del fichero Usuarios.txt).
+	int dia_clase;			//Día (Día_clase), valor numérico de 1 a 5 que indica el día de la semana que imparte clase de la materia el profesor.
+	int hora_clase;			//Hora (Hora_clase), valor numérico de 1 a 6 que indica el tramo horario en el que imparte clase de la materia el profesor.
+	char id_materia[5];		//Identificador de la materia (Id_materia), 4 dígitos (debe coincidir con el Id_materia de alguna materia del fichero Materias.txt)
+	char grupo[11];			//Grupo (Grupo) al que imparte clase, 10 caracteres máximo (debe coincidir con algún grupo de los que aparecen en el fichero Alumnos.txt).
+	
+}Horario;
+
+//Cabecera: void volcarHorarios()
+//Postcondicion: Vuelca la informacion del fichero Horarios.txt a estructuras del programa
+void volcarHorarios();
+
+//Cabecera: void annadirHoras()
+//Postcondicion: Añade horas al horario de un profesor
+void annadirHoras();
+
+//Cabecera: void eliminarHoras()
+//Postcondicion: Elimina horas del horario de un profesor
+void eliminarHoras();
+
+//Cabecera: void modificarHoras()
+//Postcondicion: Modifica horas del horario de un profesor
+void modificarHoras();
+
+//Cabecera: void listarHorarios()
+//Postcondicion: Lista horarios de un profesor
+void listarHorarios();
+
+#endif	//_HORARIOS_H_
