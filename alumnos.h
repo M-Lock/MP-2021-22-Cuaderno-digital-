fce29adefa79da7b5f37f2ac5e4b196306866362
Marcos Jiménez <99983002+M-Lock@users.noreply.github.com>
2022-03-21 00:38:41 +0100

    Update alumnos.h

--- alumnos.h
+++ alumnos.h
@@ -1,29 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #ifndef ALUMNOS_H_
 #define ALUMNOS_H_
 
 typedef struct
 {	
-	int Id_alum;
-	char Nombre_alum [21];
-	char Direc_alum [31];
-	char Local_alum [31];
-	char Curso [31];
-	char Grupo [11];
-}Alumno;
-//Postcondici髇:Crea un conjunto de estructuras alumno a partir del fichero
-void volcado_entrada_alum();
-//Postcondici髇: A馻de al alumno con los valores recibidos de la entrada estandar
-void dar_alta_alum();
-//Precondici髇: Recibe la Id del alumno a borrar
-//Postcondici髇: Elimina los datos correspondientes a dicho alumno
-void dar_baja_alum(int);
-//Precondici髇: Recibe la Id del alumno a modificar
-//Postcondici髇: Modifica los datos seleccionados correspondientes a dicho alumno
-void modif_alum(int);
+	char id_alum [7];
+	char nombre_alum [21];
+	char direc_alum [31];
+	char local_alum [31];
+	char curso [31];
+	char grupo [11];
+}alumno;
+
+alumno *v_alumnos;	//Puntero que se帽ala el comienzo de un vector dinamico del tipo alumno
+int n_alumnos;	//Numero de alumnos matriculados (Encontrados en el fichero Alumnos.txt)
+
+//Postcondici贸n:Crea un conjunto de estructuras alumno a partir del fichero
+void volcado_entrada_alum(alumno **, int *);
+//Postcondici贸n: A帽ade al alumno con los valores recibidos de la entrada estandar
+void dar_alta_alum(alumno*,int);
+//Precondici贸n: Recibe la Id del alumno a borrar
+//Postcondici贸n: Elimina los datos correspondientes a dicho alumno
+void dar_baja_alum(alumno*,int, char[]);
+//Precondici贸n: Recibe la Id del alumno a modificar
+//Postcondici贸n: Modifica los datos seleccionados correspondientes a dicho alumno
+void modif_alum(alumno*,int, char[]);
 //Postcondicion: Muestra los nombres completos de los alumnos dados de alta;
-void listar_alum(Alumno []);
-//Postcondici髇: Sobreescribe el fichero con los datos correspondientes al as estruccturas del programa
-void volcado_fichero_alum();
+void listar_alum(alumno*, int );
+//Postcondici贸n: Sobreescribe el fichero con los datos correspondientes al as estruccturas del programa
+void volcado_fichero_alum(alumno*);
 
 
 #endif
