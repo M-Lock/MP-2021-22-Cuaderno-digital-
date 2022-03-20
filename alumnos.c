70c2e52c23b1cb5986478b1e082e1e441bfb89b5
Marcos Jiménez <99983002+M-Lock@users.noreply.github.com>
2022-03-21 00:37:32 +0100

    Update alumnos.c

--- alumnos.c
+++ alumnos.c
@@ -1,36 +1,160 @@
 #include "alumnos.h"
 
-//Postcondici髇:Crea un conjunto de estructuras alumno a partir del fichero
-void volcado_entrada_alum()
+//Postcondici贸n:Crea un conjunto de estructuras alumno a partir del fichero
+void volcado_entrada_alum(alumno **v_alumnos, int *n_alumnos)
+{
+	FILE *fichero;
+	int len;
+    char linea[131]; //guardamos una linea del fichero antes de tratarla (130 es el tama帽o maximo que puede alcanzar una linea)
+    char *token=NULL; //Nos permitir谩 dividir la cadena en segmentos
+
+    *v_alumnos=NULL;
+    *n_alumnos=0;
+    
+   fichero = fopen("Alumnos.txt", "r");
+     if (fichero == NULL)
+	 {
+		printf("Error al abrir el fichero:se encuentra vacio o no existe");
+		exit(2);
+	 }	
+
+	 	do{
+	 		if ( (fgets(linea,131,fichero) ) != NULL)
+			 {     
+            
+                *v_alumnos=realloc((alumno *)(*v_alumnos),((*n_alumnos)+1)*sizeof(alumno));
+                
+              
+               //partimos la cadena en segmentos para poder almacenarla
+			    token=strtok(linea,"-");
+			   	if(token == NULL) break;
+                strcpy((*v_alumnos)[*n_alumnos].id_alum,token);
+				
+				token=strtok(NULL,"-");
+				if (token==NULL) break;
+                strcpy((*v_alumnos)[*n_alumnos].nombre_alum,token);
+				
+				token=strtok(NULL,"-");
+				if (token==NULL) break;
+                strcpy((*v_alumnos)[*n_alumnos].direc_alum,token);
+					
+				
+				token=strtok(NULL,"-");
+				if (token==NULL) break;
+            	strcpy((*v_alumnos)[*n_alumnos].local_alum,token);
+
+				token=strtok(NULL,"-");
+				if (token==NULL) break;
+            	strcpy((*v_alumnos)[*n_alumnos].curso,token);
+					
+				token=strtok(NULL,"-");
+				if (token==NULL) break;
+            	 strcpy((*v_alumnos)[*n_alumnos].curso,token);
+		  
+		    (*n_alumnos)++;
+            
+        }
+		 }while (!feof(fichero)); //Realizamos la conversion mientras nos queden lineas en el fichero
+		 fclose (fichero);
+}
+//Postcondici贸n: A帽ade al alumno con los valores recibidos de la entrada estandar
+void dar_alta_alum(alumno* v_alumnos,int n_alumnos)
 {
 	
 }
-//Postcondici髇: A馻de al alumno con los valores recibidos de la entrada estandar
-void dar_alta_alum()
+//Precondici贸n: Recibe la Id del alumno a borrar (la id debe encontrarse en la tabla mostrada y ser mayor que 000000)
+//Postcondici贸n: Elimina los datos correspondientes a dicho alumno
+void dar_baja_alum(alumno* v_alumnos,int n_alumnos,char id [])
 {
-	
+	for (int i = 0; i < n_alumnos;i++)
+	{
+		if (strcmp(id,v_alumnos[i].id_alum) == 0)
+		{
+			
+			strcpy(v_alumnos[i].id_alum,"000000");
+			i = n_alumnos;			
+		}
+	}
 }
-//Precondici髇: Recibe la Id del alumno a borrar
-//Postcondici髇: Elimina los datos correspondientes a dicho alumno
-void dar_baja_alum(int id)
+//Precondici贸n: Recibe la Id del alumno a modificar (la id debe encontrarse en la tabla mostrada y ser mayor que 000000)
+//Postcondici贸n: Modifica los datos seleccionados correspondientes a dicho alumno
+void modif_alum(alumno* v_alumnos,int n_alumnos, char id [])
 {
-	
-}
-//Precondici髇: Recibe la Id del alumno a modificar
-//Postcondici髇: Modifica los datos seleccionados correspondientes a dicho alumno
-void modif_alum(int id)
-{
-	
+	int mod;
+	for (int i = 0; i < n_alumnos;i++)
+	{
+		if (strcmp(id,v_alumnos[i].id_alum) == 0)
+		{
+			printf("驴Que desea modificar?\n 1.Nombre alumno\n 2.direccion\n 3.localidad\n4.curso\n 5.grupo ");
+			scanf("%d", &mod);
+			switch(mod)
+			{
+				case (1): ;//se pedira por pantalla y se modificar谩 si es valido.
+				char nombre_nuevo [21];
+					do
+					{	
+						printf("Introduzca el nuevo nombre, maximo 20 caracteres (pulse intro para finalizar):");
+						scanf("%s",nombre_nuevo);
+						if (strlen(nombre_nuevo) > 20) printf("El nombre no es valido");
+						else strcpy(v_alumnos[i].nombre_alum,nombre_nuevo);
+					} while (strlen(nombre_nuevo) > 20);
+					
+				case(2): ;
+				char direc_nuevo [31];
+					do
+					{
+						printf("Introduzca el nuevo nombre, maximo 30 caracteres (pulse intro para finalizar):");
+						scanf("%s",direc_nuevo);
+						if (strlen(direc_nuevo) > 30) printf("La direccion no es valida");
+						else strcpy(v_alumnos[i].direc_alum,direc_nuevo);
+					} while (strlen(direc_nuevo) > 30);
+				
+				case(3): ;
+				char local_nuevo [31];
+				do
+					{
+						printf("Introduzca el nuevo nombre, maximo 30 caracteres (pulse intro para finalizar):");
+						scanf("%s",local_nuevo);
+						if (strlen(local_nuevo) > 30) printf("El nombre no es valido");
+						else strcpy(v_alumnos[i].local_alum,nombre_nuevo);
+					} while (strlen(local_nuevo) > 30);	
+					
+				case(4): ;	
+				char curso_nuevo [31];
+				do
+					{
+						printf("Introduzca el nuevo nombre, maximo 30 caracteres (pulse intro para finalizar):");
+						scanf("%s",curso_nuevo);
+						if (strlen(curso_nuevo) > 30) printf("El nombre no es valido");
+						else strcpy(v_alumnos[i].curso,curso_nuevo);
+					} while (strlen(curso_nuevo) > 30);
+					
+				case(5): ;
+				char grupo_nuevo [11];
+				do
+					{
+						printf("Introduzca el nuevo nombre, maximo 10 caracteres (pulse intro para finalizar):");
+						scanf("%s",grupo_nuevo);
+						if (strlen(grupo_nuevo) > 10) printf("El nombre no es valido");
+						else strcpy(v_alumnos[i].grupo,nombre_nuevo);
+					} while (strlen(grupo_nuevo) > 10);
+			}	
+		}
+	}
 }
 //Postcondicion: Muestra los nombres completos de los alumnos dados de alta;
-void listar_alum( Alumno alumnos [])
+void listar_alum(alumno* v_alumnos,int n_alumnos)
 {
-	int i;
-	mientras i < tamano_vector
-		printf("%d %s %s %s %s"alumnos[n].Id_alum,alumnos)
+	 if (n_alumnos > 0)
+	 {
+	 	for(int i = 0; i < n_alumnos;i++)
+		 {
+		 	printf("%s %s %s %s %s %s",v_alumnos[i].id_alum,v_alumnos[i].nombre_alum,v_alumnos[i].direc_alum,v_alumnos[i].local_alum,v_alumnos[i].curso,v_alumnos[i].grupo);
+		 }
+	 }
 }
-//Postcondici髇: Sobreescribe el fichero con los datos correspondientes al as estruccturas del programa
-void volcado_fichero_alum()
+//Postcondici贸n: Sobreescribe el fichero con los datos correspondientes a las estruccturas del programa
+void volcado_fichero_alum(alumno* v_alumnos)
 {
 	
 }
