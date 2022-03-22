f8d96640a3430a34c1bc35980d7142ed124a9db9
David Brea Piñero <100689419+davidbp10@users.noreply.github.com>
2022-03-22 17:31:01 +0100

    Usuarios_4.c
    -Añado las cabeceras de las funciones para volcar las estructuras en el fichero.
    -Implementada y funcionando la función de volcado a fichero de Usuarios ; void volcarUsuarios(tipo_Usuario *vU, size_t tam).
    -Implementada y funcionando la función para dar de baja un usuario ; void bajaUsuario(tipo_Usuario **vUsuarios,int *nUsuarios).

--- Usuarios.c
+++ Usuarios.c
@@ -1,10 +1,14 @@
-//Implementación módulo Usuarios
+//Implementaciï¿½n mï¿½dulo Usuarios
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "Usuarios.h"
 
+
+//Cabecera: void volcarUsuarios(tipo_Usuario **, int *);
+//Precondicion: Se ponen de entrada el vector de usuarios y el numero de usuarios por referencia
+//Postcondicion: Vuelca la informacion del fichero Usuarios.txt a estructuras del programa
 void cargarUsuarios(tipo_Usuario **vUsuarios, int *n){
 	
 	FILE *fichero;
@@ -79,11 +83,45 @@ void cargarUsuarios(tipo_Usuario **vUsuarios, int *n){
 
 }
 
+
+//Cabecera: void volcarUsuarios(tipo_Usuario *vUsuarios, size_t tam);
+//Precondicion: Recibe un vector de usuarios igual o modificado y el numero de usuarios
+//Postcondicion: Vuelca en el fichero Usuarios.txt las nuevas modificaciones en los usuarios
+void volcarUsuarios(tipo_Usuario *vU, size_t tam){
+	FILE *f;
+	int i;
+	
+	//Escribe en el fichero toda la lista de usuarios
+	f = fopen ("Usuarios.txt", "w+");
+	
+	i = 0;
+	
+	if (f == NULL) puts ("\tNo se ha podido modificar el fichero");
+	else{
+		while (i < tam-1){
+			fprintf(f, "%s-%s-%s-%s-%s\n", vU[i].id, vU[i].nombre, vU[i].perfil, vU[i].usuario, vU[i].contrasenna);
+			
+			i++;
+		}
+	}
+	fprintf (f, "%s-%s-%s-%s-%s\n", vU[i].id, vU[i].nombre, vU[i].perfil, vU[i].usuario, vU[i].contrasenna);
+	
+	puts ("\tSe han volcado los usuarios correctamente.");
+	
+	fclose (f);
+	
+	system("pause");
+}
+
+
+//Cabecera: void mostrar_vUsuarios (tipo_Usuario *, int);
+//Precondicion: vector de tipo_usuario declarado y entero con el numero de usuarios
+//Postcondicion: muestra los usuarios por pantalla
 void listarUsuarios (tipo_Usuario *vUsuarios, int n) {
 	int i;
 
     if( n == 0)
-        printf("\tNo tienes horarios registrados \n");
+        printf("\tNo tienes usuarios registrados \n");
     else
         for(i=0;i<n;i++){
             printf("%d) %s-%s-%s-%s-%s\n", i+1, vUsuarios[i].id, vUsuarios[i].nombre, vUsuarios[i].perfil, vUsuarios[i].usuario, vUsuarios[i].contrasenna);
@@ -91,6 +129,10 @@ void listarUsuarios (tipo_Usuario *vUsuarios, int n) {
         }
 }
 
+
+//Cabecera:void altaUsuario(tipo_Usuario **vUsuarios,int *nUsuarios)
+//Precondicion: Recibe el vector de usuarios y el numero de estos.
+//Postcondicion: Da de alta un usuario
 void altaUsuario(tipo_Usuario **vUsuarios,int *nUsuarios){
 
     int n,i = 0;
@@ -129,7 +171,7 @@ void altaUsuario(tipo_Usuario **vUsuarios,int *nUsuarios){
     printf("\t");
     fflush(stdin);
     fgets ((*vUsuarios)[*nUsuarios].nombre, 21, stdin);
-    cambio_salto ((*vUsuarios)[*nUsuarios].nombre);
+    cambio_linea ((*vUsuarios)[*nUsuarios].nombre);
 
     while(n<1 || n>2){
 
@@ -171,14 +213,77 @@ void altaUsuario(tipo_Usuario **vUsuarios,int *nUsuarios){
     (*nUsuarios)++;
 
 }
-void bajaUsuario(){
-	printf ("Estas en baja usuario.\n");
+
+
+//cabecera:void bajaUsuario(tipo_usuario **vUsuarios,int *nUsuarios);
+//precondicion: recibe el vector usuarios (vUsuarios) y el numero total de usuarios (nUsuarios).
+//postcondicion: elimina el usuario indicado.
+void bajaUsuario(tipo_Usuario **vUsuarios,int *nUsuarios){
+
+    int i;
+    int u = 0;
+    char resp=' ';
+    char id[4];
+
+    puts("______________________________________________________________\n");
+    puts("\tDAR DE BAJA A USUARIO");
+    puts("______________________________________________________________\n");
+
+    printf("\tIntroduzca el id del usuario a eliminar: ");
+    printf("\t");
+    scanf("%3s",id);
+
+    for(i=0;i<(*nUsuarios);i++){
+
+        if(strcmp((*vUsuarios)[i].id, id)==0){
+
+            u=i;
+
+        }
+
+    }
+
+    while(resp!='s' && resp!='n'){
+
+        printf("\tSe va a eliminar el siguiente usuario: %s-%s-%s-%s-%s\n", (*vUsuarios)[u].id, (*vUsuarios)[u].nombre, (*vUsuarios)[u].perfil, (*vUsuarios)[u].usuario, (*vUsuarios)[u].contrasenna);
+        printf("\tEstas seguro de eliminar el usuario? (s/n)\n");
+        fflush(stdin);
+        printf("\t");
+        scanf("%c",&resp);
+
+    }
+
+    if(resp=='s'){
+
+        for(i=u;i+1<(*nUsuarios);i++){
+
+            strcpy((*vUsuarios)[i].id,(*vUsuarios)[i+1].id);
+            strcpy((*vUsuarios)[i].nombre,(*vUsuarios)[i+1].nombre);
+            strcpy((*vUsuarios)[i].perfil,(*vUsuarios)[i+1].perfil);
+            strcpy((*vUsuarios)[i].usuario,(*vUsuarios)[i+1].usuario);
+            strcpy((*vUsuarios)[i].contrasenna,(*vUsuarios)[i+1].contrasenna);
+
+        }
+
+        (*nUsuarios)--;
+
+        *vUsuarios=realloc((tipo_Usuario *)(*vUsuarios),(*nUsuarios)*sizeof(tipo_Usuario));
+
+    }else{
+
+        printf("\tSe ha cancelado la eliminacion del usuario\n");
+
+    }
+
 }
+
+
+
 void modificarUsuarios(){
 	printf ("Estas en modificar usuario.\n");
 }
 
-void cambio_salto (char * nombre){
+void cambio_linea (char * nombre){
 	if ((strlen(nombre) > 0) && (nombre[strlen(nombre) - 1] == '\n'))
     {
         nombre[strlen(nombre) - 1] = '\0';
