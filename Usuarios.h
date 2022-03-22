2b7379f5cfb517d99d8811dbcfa6f4c085c92c96
David Brea Piñero <100689419+davidbp10@users.noreply.github.com>
2022-03-22 17:31:41 +0100

    Usuarios.h_4.c
    -Añado las cabeceras de las funciones para volcar las estructuras en el fichero.
    -Implementada y funcionando la función de volcado a fichero de Usuarios ; void volcarUsuarios(tipo_Usuario *vU, size_t tam).
    -Implementada y funcionando la función para dar de baja un usuario ; void bajaUsuario(tipo_Usuario **vUsuarios,int *nUsuarios).

--- Usuarios.h
+++ Usuarios.h
@@ -1,36 +1,33 @@
-//Declaraciones módulo Usuarios
+//Declaraciones mï¿½dulo Usuarios
 
 #ifndef _USUARIOS_H_
 #define _USUARIOS_H_
 
 typedef struct{
-	char id[4];		//Identificador del usuario, con 3 dígitos
-	char nombre[21];	//Nombre completo del usuario, con 20 caracteres máximo.
-	char perfil[14];	//Perfil del usuario (Perfil_usuario): «administrador» o «profesor».
+	char id[4];		//Identificador del usuario, con 3 dï¿½gitos
+	char nombre[21];	//Nombre completo del usuario, con 20 caracteres mï¿½ximo.
+	char perfil[14];	//Perfil del usuario (Perfil_usuario): ï¿½administradorï¿½ o ï¿½profesorï¿½.
 	char usuario[6];		//Nombre de usuario (Usuario) para acceder al sistema, 5 caracteres.
-	char contrasenna[9];	//Contraseña para acceder al sistema (Contraseña), 8 caracteres.
+	char contrasenna[9];	//Contraseï¿½a para acceder al sistema (Contraseï¿½a), 8 caracteres.
 } tipo_Usuario;
 
-tipo_Usuario *vUsuarios;		//Vector usuario donde se almacenará cada uno de los usuarios del fichero y el cual se modificará
+tipo_Usuario *vUsuarios;		//Vector usuario donde se almacenarï¿½ cada uno de los usuarios del fichero y el cual se modificarï¿½
 int nUsuarios;		//numero de usuarios
 
-//Cabecera: void volcarUsuarios(tipo_Usuario **, int *);
-//Precondicion: Se ponen de entrada el vector de usuarios y el numero de usuarios por referencia
-//Postcondicion: Vuelca la informacion del fichero Usuarios.txt a estructuras del programa
+
 void cargarUsuarios(tipo_Usuario **, int *);
 
-//Cabecera: void mostrar_vUsuarios (tipo_Usuario *, int);
-//Precondicion: vector de tipo_usuario declarado y entero con el numero de usuarios
-//Postcondicion: muestra los usuarios por pantalla
+
+void volcarUsuarios(tipo_Usuario *vUsuarios, size_t tam);
+
+
 void listarUsuarios (tipo_Usuario *, int);
 
-//Cabecera:void altaUsuario()
-//Postcondicion: Da de alta un usuario
+
 void altaUsuario(tipo_Usuario **, int *);
 
-//Cabecera: void bajaUsuario()
-//Postcondicion: Da de baja un usuario
-void bajaUsuario();
+
+void bajaUsuario(tipo_Usuario **,int *);
 
 //Cabecera: void modificarUsuarios()
 //Postcondicion: modifica usuarios
@@ -40,6 +37,6 @@ void modificarUsuarios();
 //Postcondicion: Lista los usuarios
 void listarUsuarios();
 
-void cambio_linea(char*)
+void cambio_linea(char*);
 
 #endif	//_USUARIOS_H_
